515c95ff61c8fc26a3338acf838499326ab4e017
Matthieu Gay-Bellile <matthieu.gb@icloud.com>
2014-02-06 16:55:39 +0100

    Cleaned out old, commented out rendering code. Simulation runs @ 26 FPS with 1000 particles.

--- FluidSim/FLSLiquid.mm
+++ FluidSim/FLSLiquid.mm
@@ -36,33 +36,6 @@ typedef struct FLSParticle
 FLSParticle;
 
 
-//// BEGIN TEMPORARY / DEBUG RENDERING CODE
-//
-//
-//typedef struct {
-//    CGPoint geometryVertex;
-//    CGPoint textureVertex;
-//} TexturedVertex;
-//
-//typedef struct {
-//    TexturedVertex bl;
-//    TexturedVertex br;
-//    TexturedVertex tl;
-//    TexturedVertex tr;
-//} TexturedQuad;
-//
-//@interface FLSLiquid()
-//
-//@property (strong) GLKBaseEffect * effect;
-//@property (assign) TexturedQuad quad;
-//@property (strong) GLKTextureInfo * textureInfo;
-//
-//@end
-//
-//
-//// END TEMPORARY / DEBUG RENDERING CODE
-
-
 @interface FLSLiquid()
 
 // Properties
@@ -72,15 +45,6 @@ FLSParticle;
 
 
 @implementation FLSLiquid
-
-
-//// BEGIN TEMPORARY / DEBUG RENDERING CODE
-//
-//
-//CGSize contentSize;
-//
-//
-//// END TEMPORARY / DEBUG RENDERING CODE
 
 
 const float DT = 1.0f / 60.0f;
@@ -299,66 +263,9 @@ int getGridY(float y) {
     return (int)floorf(y / CELL_SIZE);
 }
 
-
-//// BEGIN TEMPORARY / DEBUG RENDERING CODE
-//
-//
-//GLKMatrix4 modelMatrix(float x, float y) {
-//    
-//    GLKMatrix4 modelMatrix = GLKMatrix4Identity;
-//    modelMatrix = GLKMatrix4Translate(modelMatrix, x*scale, y*scale, 0);
-//    modelMatrix = GLKMatrix4Translate(modelMatrix, -contentSize.width/2, -contentSize.height/2, 0);
-//    return modelMatrix;
-//    
-//}
-//
-//
-//// END TEMPORARY / DEBUG RENDERING CODE
-
-
 -(id)initWithScreenSize:(GLKVector2)screenSize {
     
     if ((self = [super init])) {
-        
-        
-//        // BEGIN TEMPORARY / DEBUG RENDERING CODE
-//        
-//        
-//        // 1
-//        self.effect = effect;
-//        
-//        // 2
-//        NSDictionary * options = [NSDictionary dictionaryWithObjectsAndKeys:
-//                                  [NSNumber numberWithBool:YES],
-//                                  GLKTextureLoaderOriginBottomLeft,
-//                                  nil];
-//        
-//        // 3
-//        NSError * error;
-//        NSString *path = [[NSBundle mainBundle] pathForResource:fileName ofType:nil];
-//        // 4
-//        self.textureInfo = [GLKTextureLoader textureWithContentsOfFile:path options:options error:&error];
-//        if (self.textureInfo == nil) {
-//            NSLog(@"Error loading file: %@", [error localizedDescription]);
-//            return nil;
-//        }
-//        
-//        contentSize = CGSizeMake(self.textureInfo.width, self.textureInfo.height);
-//        
-//        TexturedQuad newQuad;
-//        newQuad.bl.geometryVertex = CGPointMake(0, 0);
-//        newQuad.br.geometryVertex = CGPointMake(self.textureInfo.width, 0);
-//        newQuad.tl.geometryVertex = CGPointMake(0, self.textureInfo.height);
-//        newQuad.tr.geometryVertex = CGPointMake(self.textureInfo.width, self.textureInfo.height);
-//        
-//        newQuad.bl.textureVertex = CGPointMake(0, 0);
-//        newQuad.br.textureVertex = CGPointMake(1, 0);
-//        newQuad.tl.textureVertex = CGPointMake(0, 1);
-//        newQuad.tr.textureVertex = CGPointMake(1, 1);
-//        self.quad = newQuad;
-//        
-//        
-//        // END TEMPORARY / DEBUG RENDERING CODE
         
         _screenSize = screenSize;
         
@@ -412,30 +319,6 @@ int getGridY(float y) {
 }
 -(void)render {
     
-//    for (size_t i : _activeParticles) {
-//        
-//        // 1
-//        self.effect.texture2d0.name = self.textureInfo.name;
-//        self.effect.texture2d0.enabled = YES;
-//        
-//        // 2
-//        self.effect.transform.modelviewMatrix = modelMatrix(_liquid[i].position.x, _liquid[i].position.y);
-//        [self.effect prepareToDraw];
-//        
-//        // 3
-//        glEnableVertexAttribArray(GLKVertexAttribPosition);
-//        glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
-//        
-//        // 4
-//        long offset = (long)&_quad;
-//        glVertexAttribPointer(GLKVertexAttribPosition, 2, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void *) (offset + offsetof(TexturedVertex, geometryVertex)));
-//        glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void *) (offset + offsetof(TexturedVertex, textureVertex)));
-//        
-//        // 5
-//        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-//        
-//    }
-    
     // 1
     // Create Projection Matrix
     GLKMatrix4 projectionMatrix = GLKMatrix4MakeScale(2*scale/_screenSize.x, 2*scale/_screenSize.y, 1.0f);
@@ -446,12 +329,9 @@ int getGridY(float y) {
     glUniform1f(self.liquidParticleShader.uRetinaScale, retinaScale);
     
     
-    
-    glBufferData(                                       // Fill bound buffer with particles
-                 GL_ARRAY_BUFFER,                       // Buffer target
-                 sizeof(_particlePositions),                       // Buffer data size
-                 _particlePositions,                               // Buffer data pointer
-                 GL_DYNAMIC_DRAW);                      // Usage - Data never changes; used for drawing
+    // 2.5
+    // Re-send vertex position data to GPU
+    glBufferData(GL_ARRAY_BUFFER, sizeof(_particlePositions), _particlePositions, GL_DYNAMIC_DRAW);
     
     
     
@@ -489,7 +369,7 @@ int getGridY(float y) {
                  GL_ARRAY_BUFFER,                       // Buffer target
                  sizeof(_particlePositions),                       // Buffer data size
                  _particlePositions,                               // Buffer data pointer
-                 GL_DYNAMIC_DRAW);                      // Usage - Data never changes; used for drawing
+                 GL_DYNAMIC_DRAW);                      // Usage - Data changes
     
 }
 -(void)createParticleWithPosX:(float)posX posY:(float)posY {
