df52718a7f22f90fe10b3bc16337a79b7f781c42
Matthieu Gay-Bellile <matthieu.gb@icloud.com>
2014-02-07 17:23:08 +0100

    Preliminary multithreading implemented. Works, but ~4x slower than singletheaded -_-

--- FluidSim/FLSLiquid.mm
+++ FluidSim/FLSLiquid.mm
@@ -8,7 +8,9 @@
 
 #import "FLSLiquid.h"
 #import "FLSLiquidParticleShader.h"
+#import <dispatch/dispatch.h>
 #import <unordered_set>
+#import <set>
 #import <unordered_map>
 
 using namespace std;
@@ -28,6 +30,9 @@ typedef struct FLSParticle
     GLKVector2 velocity;
     BOOL alive;
     int index;
+    
+    float p;
+    float pnear;
     
     int neighborCount;
     int ci;
@@ -68,9 +73,17 @@ GLKVector2 _screenSize;
 
 FLSParticle _liquid[MAX_PARTICLES];
 GLKVector2 _particlePositions[MAX_PARTICLES];
-unordered_set<size_t> _activeParticles;
+set<size_t> _activeParticles;
 
 unordered_map<size_t, unordered_map<size_t, unordered_set<size_t>>> _grid;
+
+dispatch_queue_t _globalQueue;
+dispatch_queue_t _accumulatedDeltaQueue;
+
+dispatch_group_t _prepareSimGroup;
+dispatch_group_t _calcPressureGroup;
+dispatch_group_t _calcForceGroup;
+dispatch_group_t _moveParticleGroup;
 
 void createParticle(float posX, float posY) {
     
@@ -121,108 +134,113 @@ void createParticle(float posX, float posY) {
         }
     }
 }
-void applyLiquidConstraints() {
+void prepareSimulationForParticle(size_t index) {
     
-    // Prepare simulation
-    for (size_t index : _activeParticles) {
-        
-        FLSParticle *particle = &_liquid[index];
-        
-        // Scale positions and velocities
-        _scaledPositions[index] = GLKVector2MultiplyScalar(particle->position, MULTIPLIER);
-        _scaledVelocities[index] = GLKVector2MultiplyScalar(particle->velocity, MULTIPLIER);
-        
-        // Reset deltas
-        _delta[index] = GLKVector2Make(0, 0);
-    }
-    for (size_t index : _activeParticles) {
-        
-        FLSParticle *particle = &_liquid[index];
-        
-        findNeighbors(*particle);
-        
-        // Calculate pressure
-        float p = 0.0f;
-        float pnear = 0.0f;
-        for (int a = 0; a < particle->neighborCount; a++) {
-            
-            GLKVector2 relativePosition = GLKVector2Subtract(_scaledPositions[particle->neighbors[a]], _scaledPositions[index]);
-            float distanceSq = relativePosition.v[0] * relativePosition.v[0] + relativePosition.v[1] * relativePosition.v[1];
-            
-            //within idealRad check
-            if (distanceSq < IDEAL_RADIUS_SQ) {
-                particle->distances[a] = sqrtf(distanceSq);
-                //if (particle.distances[a] < Settings.EPSILON) particle.distances[a] = IDEAL_RADIUS - .01f;
-                float oneminusq = 1.0f - (particle->distances[a] / IDEAL_RADIUS);
-                p = (p + oneminusq * oneminusq);
-                pnear = (pnear + oneminusq * oneminusq * oneminusq);
-            }
-            else {
-                particle->distances[a] = MAXFLOAT;
-            }
-        }
-        
-        // Apply forces
-        float pressure = (p - 5.0f) / 2.0f; //normal pressure term
-        float presnear = pnear / 2.0f; //near particles term
-        GLKVector2 change = GLKVector2Make(0, 0);
-        for (int a = 0; a < particle->neighborCount; a++)
-        {
-            GLKVector2 relativePosition = GLKVector2Subtract(_scaledPositions[particle->neighbors[a]], _scaledPositions[index]);
-            
-            if (particle->distances[a] < IDEAL_RADIUS)
-            {
-                float q = particle->distances[a] / IDEAL_RADIUS;
-                float oneminusq = 1.0f - q;
-                float factor = oneminusq * (pressure + presnear * oneminusq) / (2.0F * particle->distances[a]);
-                GLKVector2 d = GLKVector2MultiplyScalar(relativePosition, factor);
-                GLKVector2 relativeVelocity = GLKVector2Subtract(_scaledVelocities[particle->neighbors[a]], _scaledVelocities[index]);
-                factor = VISCOSITY * oneminusq * DT;
-                d = GLKVector2Subtract(d, GLKVector2MultiplyScalar(relativeVelocity, factor));
-                _delta[particle->neighbors[a]] = GLKVector2Add(_delta[particle->neighbors[a]], d);
-                change = GLKVector2Subtract(change, d);
-            }
-        }
-        _delta[index] = GLKVector2Add(_delta[index], change);
-    }
+    FLSParticle *particle = &_liquid[index];
     
-    // Move particles
-    for (size_t index : _activeParticles) {
+    // Find neighbors
+    findNeighbors(*particle);
+    
+    // Scale positions and velocities
+    _scaledPositions[index] = GLKVector2MultiplyScalar(particle->position, MULTIPLIER);
+    _scaledVelocities[index] = GLKVector2MultiplyScalar(particle->velocity, MULTIPLIER);
+    
+    // Reset deltas
+    _delta[index] = GLKVector2Make(0, 0);
+    
+    // Reset pressures
+    particle->p = 0;
+    particle->pnear = 0;
+    
+}
+void calculatePressureForParticle(size_t index) {
+    
+    FLSParticle *particle = &_liquid[index];
+    
+    // Calculate pressure
+    for (int a = 0; a < particle->neighborCount; a++) {
         
-        FLSParticle *particle = &_liquid[index];
+        GLKVector2 relativePosition = GLKVector2Subtract(_scaledPositions[particle->neighbors[a]], _scaledPositions[index]);
+        float distanceSq = relativePosition.v[0] * relativePosition.v[0] + relativePosition.v[1] * relativePosition.v[1];
         
-        particle->position = GLKVector2Add(particle->position, GLKVector2MultiplyScalar(_delta[index], 1 / MULTIPLIER));
-        _particlePositions[particle->index] = particle->position;
-        particle->velocity = GLKVector2Add(particle->velocity, GLKVector2MultiplyScalar(_delta[index], 1 / (MULTIPLIER * DT)));
-        
-        // Update particle cell
-        int x = getGridX(particle->position.x);
-        int y = getGridY(particle->position.y);
-        
-        if (particle->ci == x && particle->cj == y)
-            continue;
+        //within idealRad check
+        if (distanceSq < IDEAL_RADIUS_SQ) {
+            particle->distances[a] = sqrtf(distanceSq);
+            //if (particle.distances[a] < Settings.EPSILON) particle.distances[a] = IDEAL_RADIUS - .01f;
+            float oneminusq = 1.0f - (particle->distances[a] / IDEAL_RADIUS);
+            particle->p = (particle->p + oneminusq * oneminusq);
+            particle->pnear = (particle->pnear + oneminusq * oneminusq * oneminusq);
+        }
         else {
-            
-            _grid.at(particle->ci).at(particle->cj).erase(index);
-            
-            if (_grid.at(particle->ci).at(particle->cj).empty()) {
-                
-                _grid.at(particle->ci).erase(particle->cj);
-                
-                if (_grid.at(particle->ci).empty()) {
-                    _grid.erase(particle->ci);
-                }
-            }
-            
-            if (_grid.find(x) == _grid.end())
-                _grid.insert(make_pair(x, unordered_map<size_t, unordered_set<size_t>>()));
-            if (_grid.at(x).find(y) == _grid.at(x).end())
-                _grid.at(x).insert(make_pair(y, unordered_set<size_t>()));
-            _grid.at(x).at(y).insert(index);
-            particle->ci = x;
-            particle->cj = y;
+            particle->distances[a] = MAXFLOAT;
         }
     }
+    
+}
+void calculateForceForParticle(int index, GLKVector2 *accumulatedDelta) {
+    
+    FLSParticle *particle = &_liquid[index];
+    
+    // Apply forces
+    float pressure = (particle->p - 5.0f) / 2.0f; //normal pressure term
+    float presnear = particle->pnear / 2.0f; //near particles term
+    GLKVector2 change = GLKVector2Make(0, 0);
+    for (int a = 0; a < particle->neighborCount; a++)
+    {
+        GLKVector2 relativePosition = GLKVector2Subtract(_scaledPositions[particle->neighbors[a]], _scaledPositions[index]);
+        
+        if (particle->distances[a] < IDEAL_RADIUS)
+        {
+            float q = particle->distances[a] / IDEAL_RADIUS;
+            float oneminusq = 1.0f - q;
+            float factor = oneminusq * (pressure + presnear * oneminusq) / (2.0F * particle->distances[a]);
+            GLKVector2 d = GLKVector2MultiplyScalar(relativePosition, factor);
+            GLKVector2 relativeVelocity = GLKVector2Subtract(_scaledVelocities[particle->neighbors[a]], _scaledVelocities[index]);
+            factor = VISCOSITY * oneminusq * DT;
+            d = GLKVector2Subtract(d, GLKVector2MultiplyScalar(relativeVelocity, factor));
+            accumulatedDelta[particle->neighbors[a]] = GLKVector2Add(accumulatedDelta[particle->neighbors[a]], d);
+            change = GLKVector2Subtract(change, d);
+        }
+    }
+    accumulatedDelta[index] = GLKVector2Add(accumulatedDelta[index], change);
+
+}
+void moveParticle(int index) {
+    
+    FLSParticle *particle = &_liquid[index];
+    
+    particle->position = GLKVector2Add(particle->position, GLKVector2MultiplyScalar(_delta[index], 1 / MULTIPLIER));
+    _particlePositions[particle->index] = particle->position;
+    particle->velocity = GLKVector2Add(particle->velocity, GLKVector2MultiplyScalar(_delta[index], 1 / (MULTIPLIER * DT)));
+    
+    // Update particle cell
+    int x = getGridX(particle->position.x);
+    int y = getGridY(particle->position.y);
+    
+    if (particle->ci == x && particle->cj == y)
+        return;
+    else {
+        
+        _grid.at(particle->ci).at(particle->cj).erase(index);
+        
+        if (_grid.at(particle->ci).at(particle->cj).empty()) {
+            
+            _grid.at(particle->ci).erase(particle->cj);
+            
+            if (_grid.at(particle->ci).empty()) {
+                _grid.erase(particle->ci);
+            }
+        }
+        
+        if (_grid.find(x) == _grid.end())
+            _grid.insert(make_pair(x, unordered_map<size_t, unordered_set<size_t>>()));
+        if (_grid.at(x).find(y) == _grid.at(x).end())
+            _grid.at(x).insert(make_pair(y, unordered_set<size_t>()));
+        _grid.at(x).at(y).insert(index);
+        particle->ci = x;
+        particle->cj = y;
+    }
+    
 }
 void findNeighbors(FLSParticle &particle) {
     particle.neighborCount = 0;
@@ -269,6 +287,14 @@ int getGridY(float y) {
         
         _screenSize = screenSize;
         
+        _globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        _accumulatedDeltaQueue = dispatch_queue_create("com.sleeples5.AccumulatedDeltaQueue", NULL);
+        
+        _prepareSimGroup = dispatch_group_create();
+        _calcPressureGroup = dispatch_group_create();
+        _calcForceGroup = dispatch_group_create();
+        _moveParticleGroup = dispatch_group_create();
+        
         if ([[UIScreen mainScreen] respondsToSelector:@selector(displayLinkWithTarget:selector:)] &&
             ([UIScreen mainScreen].scale == 2.0))
         {
@@ -279,7 +305,7 @@ int getGridY(float y) {
         
         [self loadShader];
         
-        _activeParticles = unordered_set<size_t>();
+        _activeParticles = set<size_t>();
         
         _grid = unordered_map<size_t, unordered_map<size_t, unordered_set<size_t>>>();
         
@@ -314,7 +340,39 @@ int getGridY(float y) {
 }
 -(void)update {
     
-    applyLiquidConstraints();
+    for (size_t index : _activeParticles) {
+        dispatch_group_async(_prepareSimGroup, _globalQueue, ^{
+            prepareSimulationForParticle(index);
+        });
+    }
+    dispatch_group_wait(_prepareSimGroup, DISPATCH_TIME_FOREVER);
+    for (size_t index : _activeParticles) {
+        dispatch_group_async(_calcPressureGroup, _globalQueue, ^{
+            calculatePressureForParticle(index);
+        });
+    }
+    dispatch_group_wait(_calcPressureGroup, DISPATCH_TIME_FOREVER);
+    for (size_t index : _activeParticles) {
+        dispatch_group_async(_calcForceGroup, _globalQueue, ^{
+            
+            GLKVector2 accumulatedDelta[MAX_PARTICLES] = {0};
+            calculateForceForParticle(index, accumulatedDelta);
+            GLKVector2 *accumulatedDeltaAsPointer = accumulatedDelta;
+            dispatch_sync(_accumulatedDeltaQueue, ^{
+                for (size_t index : _activeParticles) {
+                    _delta[index] = GLKVector2Add(_delta[index], accumulatedDeltaAsPointer[index]);
+                }
+            });
+            
+        });
+    }
+    dispatch_group_wait(_calcForceGroup, DISPATCH_TIME_FOREVER);
+    for (size_t index : _activeParticles) {
+        
+        moveParticle(index);
+        
+    }
+    //applyLiquidConstraints();
     
 }
 -(void)render {
